c673a290949683e98c7d6067c4ef051a1cfb0efc
Howell <th15817161961@gmail.com>
2017-06-24 21:02:11 +0800

    start

--- Cloud-Web/src/main/java/run/AppServer.java
+++ Cloud-Web/src/main/java/run/AppServer.java
@@ -1,6 +1,9 @@
+package run;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by HowellYang on 19/6/17 PM2:27.
@@ -8,6 +11,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan("controller")
 public class AppServer {
     public static void main(String[] args) {
         SpringApplication.run(AppServer.class, args);
