c55c2d5351203dc294c1472c06b30d5aff892b09
Howell <th15817161961@gmail.com>
2017-06-25 21:46:52 +0800

    start

--- Cloud-Web/src/main/java/run/AppServer.java
+++ Cloud-Web/src/main/java/run/AppServer.java
@@ -2,7 +2,8 @@ package run;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -10,8 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
  * E-Mail:th15817161961@gmail.com
  */
 @SpringBootApplication
-@EnableDiscoveryClient
-@ComponentScan("controller")
+@EnableEurekaServer
 public class AppServer {
     public static void main(String[] args) {
         SpringApplication.run(AppServer.class, args);
